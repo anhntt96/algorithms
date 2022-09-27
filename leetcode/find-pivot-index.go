3b73e29fcfd9dbc51508656d49e47f3044da5159
tuananh.nguyentien <tuananh.nguyentien@grabtaxi.com>
2022-09-27 21:29:39 +0700

    find-pivot-index

---
+++ leetcode/find-pivot-index.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-pivot-index/
+func pivotIndex(nums []int) int {
+	leftSum := make([]int, len(nums))
+	n := len(nums)
+	for i, v := range nums {
+		if i > 0 {
+			leftSum[i] = leftSum[i-1]
+		}
+		leftSum[i] += v
+
+	}
+	sum := leftSum[n-1]
+
+	for i := 0; i < len(nums); i++ {
+		if leftSum[i]-nums[i] == sum-leftSum[i] {
+			return i
+		}
+	}
+	return -1
+}
